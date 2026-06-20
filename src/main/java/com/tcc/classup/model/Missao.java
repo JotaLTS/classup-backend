5f14159926419d519a071a709734c375f923ca59
Jonatas <208494109+JotaLTS@users.noreply.github.com>
2026-06-20 15:12:40 -0300

    feat: adiciona a entidade Missao

---
+++ src/main/java/com/tcc/classup/model/Missao.java
@@ -0,0 +1,60 @@
+package com.tcc.classup.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_missao")
+@Getter
+@NoArgsConstructor
+public class Missao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @NotBlank
+    @Column(nullable = false,length = 100)
+    private String titulo;
+
+    @Setter
+    @NotBlank
+    @Column(nullable = false,length = 500)
+    private String descricao;
+
+    @Setter
+    @NotNull
+    @DecimalMin("0.0")
+    @DecimalMax("10.0")
+    @Column(nullable = false)
+    private Double valor;
+
+    @CreationTimestamp
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @Setter
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime prazoEntrega;
+
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "criador_id",nullable = false)
+    private Usuario criador;
+
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "disciplina_id",nullable = false)
+    private Disciplina disciplina;
+
+}
