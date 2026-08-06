52d291b1703836a29307032a9ccbfaadc5aae6c7
Jonatas <208494109+JotaLTS@users.noreply.github.com>
2026-08-06 09:17:38 -0300

    feat: adiciona Service da entidade Professor

---
+++ src/main/java/com/tcc/classup/service/professor/ProfessorService.java
@@ -0,0 +1,17 @@
+package com.tcc.classup.service.professor;
+
+
+import com.tcc.classup.dto.professor.ProfessorCreateDTO;
+import com.tcc.classup.dto.professor.ProfessorResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProfessorService {
+
+    ProfessorResponseDTO criar(ProfessorCreateDTO dto);
+
+    ProfessorResponseDTO buscarPorId(Long id);
+
+    Page<ProfessorResponseDTO> listar(Pageable pageable);
+    
+}
