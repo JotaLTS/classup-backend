a5efff41bb240f2b22004bdeb5579845d2d77209
Jonatas <208494109+JotaLTS@users.noreply.github.com>
2026-08-07 00:19:30 -0300

    feat: adiciona metodo no TurmaRepository

--- src/main/java/com/tcc/classup/repository/TurmaRepository.java
+++ src/main/java/com/tcc/classup/repository/TurmaRepository.java
@@ -1,10 +1,17 @@
 package com.tcc.classup.repository;
 
 
+import com.tcc.classup.enums.TipoTurma;
 import com.tcc.classup.model.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TurmaRepository  extends JpaRepository<Turma,Long> {
+
+    Optional<Turma> findByTipoTurmaAndAnoAndSemestre(TipoTurma tipoTurma, Integer ano, Integer semestre);
+
+
 }
